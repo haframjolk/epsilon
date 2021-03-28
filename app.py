5c149a591a198eb4a25f2eac99566455c26ae27f
Reyn Alpha <reynalpha@outlook.com>
2021-03-28 15:28:25 +0000

    Allow checkbox voting

--- app.py
+++ app.py
@@ -2,39 +2,67 @@
 from flask import Flask, render_template, request
 import json
 
+
+def init():
+    """Initializes required data"""
+    global config
+    global votes
+
+    config = {}
+    with open("config.json") as f:
+        config = json.load(f)
+
+    votes = {candidate: 0 for candidate in config["candidates"]}
+    votes["empty"] = 0
+
+
+# Initialize data
+init()
+# Initialize app
 app = Flask(__name__)
-
-config = {}
-with open("config.json") as f:
-    config = json.load(f)
-
-votes = {candidate: 0 for candidate in config["candidates"]}
-votes["empty"] = 0
 
 
 def write_json():
+    """Writes current vote tally to JSON file"""
     with open("votes.json", "w") as f:
         json.dump(votes, f)
 
 
-def vote_for(candidate):
-    if candidate not in votes:
-        return False
-    else:
-        votes[candidate] += 1
-        write_json()
+def vote_for(candidate: str):
+    """Vote for a single candidate"""
+    votes[candidate] += 1
+
+
+def candidate_exists(candidate: str):
+    """Returns True if candidate exists, False otherwise"""
+    return candidate in votes
+
+
+def vote_for_candidates(candidates: list[str]):
+    """Vote for multiple candidates. Returns True if successful, False otherwise"""
+    # If candidates are not selected, register empty vote
+    if not candidates:
+        empty_vote()
         return True
+    # If all candidates exist, vote for every one
+    if all(candidate_exists(candidate) for candidate in candidates):
+        for candidate in candidates:
+            vote_for(candidate)
+        return True
+    return False
+
+
+def remove_password(password: str):
+    config["passwords"].remove(password)
 
 
 def empty_vote():
     votes["empty"] += 1
 
-    write_json()
-
 
 @app.route("/")
 def index():
-    return render_template("index.html", candidates=config["candidates"])
+    return render_template("index.html", candidates=config["candidates"], multiple=config["multiple"])
 
 
 @app.route("/vote", methods=["POST"])
@@ -44,21 +72,15 @@ def vote():
     success = False
     # Check password validity
     if password in config["passwords"]:
-        candidate = data.get("candidate")
-
-        # If no candidate was selected, register an empty vote
-        if candidate is None:
-            empty_vote()
-            success = True
-        # Else, try to vote for candidate
-        elif vote_for(candidate):
-            # If successful, remove password
-            config["passwords"].remove(password)
-            success = True
-        else:
-            # invalid candidate selected, return error
-            pass
+        candidates = [data.get("candidate")]
+        # Get list only if multiple is selected, to prevent multiple votes
+        if config["multiple"]:
+            candidates = data.getlist("candidate")
+        # Try to vote for candidates
+        success = vote_for_candidates(candidates)
     if success:
+        remove_password(password)
+        write_json()
         return render_template("success.html")
     else:
         return render_template("error.html")
